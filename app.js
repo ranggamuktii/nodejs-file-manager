7356b1704bd8da66f3aec19689c8fe7caf2d2546
Rangga Mukti <r4n994mukti@gmail.com>
2024-10-26 00:05:17 +0700

    Membuat fitur read-folder : membaca isi sebuah folder

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const fs = require('node:fs');
 const readline = require('node:readline');
+const path = require('path');
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -59,6 +60,53 @@ app.extSorter = () => {
       moveFile(extName, file);
     }
   }
+};
+
+app.readFolder = () => {
+  rl.question('Masukan Nama Folder : ', (folderName) => {
+    const pathFolder = `${__dirname}/${folderName}`;
+
+    const result = [];
+
+    if (fs.existsSync(pathFolder)) {
+      console.log('folder exists');
+    } else {
+      console.log('folder not found!');
+      rl.close();
+    }
+
+    const listFile = fs.readdirSync(pathFolder);
+
+    for (let index = 0; index < listFile.length; index++) {
+      const file = listFile[index];
+
+      const detail = fs.statSync(pathFolder + '/' + file);
+      const extName = file.split('.')[file.split('.').length - 1];
+      const ukuranFileBytes = detail.size >= 1024 * 1024 ? (detail.size / (1024 * 1024)).toFixed(2) + 'mb' : (detail.size / 1024).toFixed(2) + 'kb';
+
+      let typeFile = '';
+
+      if (['jpg', 'png'].includes(extName)) {
+        typeFile = 'gambar';
+      } else if (['txt', 'md'].includes(extName)) {
+        typeFile = 'text';
+      } else if (['pdf'].includes(extName)) {
+        typeFile = 'document';
+      } else {
+        typeFile = 'Tidak terdeteksi';
+      }
+
+      result.push({
+        namaFile: file,
+        extensi: extName,
+        jenisFile: typeFile,
+        tanggalDibuat: detail.birthtime.toISOString().split('T')[0],
+        ukuranFile: ukuranFileBytes,
+      });
+    }
+    console.log(result);
+    rl.close();
+  });
 };
 
 module.exports = app;
--- index.js
+++ index.js
@@ -14,6 +14,9 @@ switch (command) {
   case 'ext-sorter':
     app.extSorter();
     break;
+  case 'read-folder':
+    app.readFolder();
+    break;
   default:
     throw Error('Invalid command');
     break;
